1e031bc0833b1f90b2a185e6b05593000b5fcd79
sahilbista205@gmail.com <sahilbista205@gmail.com>
2025-09-16 20:43:10 +0545

    Feat : Creted a mongoose model to store the URL related datas

---
+++ models/URL.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.schema({
+  originalURL: {
+    type: String,
+    required: true,
+  },
+  shortCode: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  clickCount: {
+    type: Number,
+    default: 0,
+  },
+});
+
+export const URLModel = mongoose.model('URL', schema);
+
+
